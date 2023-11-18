2427dc9039a4c2178e0d104280d2efb32b2f36b0
AilsaYang <ailsayang831031@gmail.com>
2023-11-18 13:44:55 +0800

    feat: set true and false of the classes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import className from "classnames";
 
 function Button() {
   const classes = className(
-    "px-3",
-    "py-1.5",
+    "px-3 py-1.5",
     {
-      "bg-sky-400": true,
+      "bg-sky-400": false,
+      "bg-sky-100": true,
     },
-    "border",
-    "border",
-    "border-blue-600",
-    "text-white"
+    "border border-blue-600",
+    { "text-white": false, "text-blue": true }
   );
   return <button className={classes}>Ailsa</button>;
 }
