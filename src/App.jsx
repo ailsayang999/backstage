363290f93082bcd4419ea31506fee03b87903190
AilsaYang <ailsayang831031@gmail.com>
2023-11-18 13:33:25 +0800

    feat: install classnames

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "App.scss";
+import className from "classnames"
+
+const finalClassName = className("px-3", "py-1.5")
+
+
+function Button() {
+  let baseClassName =
+    "px-3 py-1.5 border  border-blue-600 bg-sky-400 text-white";
+  return <button className={baseClassName}>{finalClassName}</button>;
+}
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <Button />;
 }
 
 export default App;
