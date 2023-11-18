a2f9313600357bff5adb70d75f875bb52c264cc2
AilsaYang <ailsayang831031@gmail.com>
2023-11-18 21:51:34 +0800

    doc: login page doc

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -31,6 +31,7 @@ const AdminLogin = () => {
             <h1 className="text-gray-800 font-bold text-2xl text-center mb-6">
               Admin Login
             </h1>
+            {/* Email address input */}
             <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -54,6 +55,8 @@ const AdminLogin = () => {
                 placeholder="Email Address"
               />
             </div>
+
+            {/* Password input */}
             <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,12 +78,16 @@ const AdminLogin = () => {
                 placeholder="Password"
               />
             </div>
+
+            {/* Submit button */}
             <button
               type="submit"
               className="block w-full bg-cyan-500 mt-5 py-2 rounded-2xl hover:bg-sky-400 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
             >
               Login
             </button>
+
+            {/* Forget and sign up */}
             <div className="flex justify-between mt-4">
               <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">
                 Forgot Password ?
@@ -93,6 +100,7 @@ const AdminLogin = () => {
                 Don't have an account yet?
               </a>
             </div>
+
           </form>
         </div>
       </div>
