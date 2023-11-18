87fbccdd5b525f529ed5d9a1affc4299ea03f2d3
AilsaYang <ailsayang831031@gmail.com>
2023-11-18 22:47:06 +0800

    feat: add input

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,6 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAuth } from "contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AdminLogin = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { login, isAuthenticated } = useAuth();
+
+  const handleEmailInputChange = (eventValue) => {
+    console.log(eventValue);
+    setUserName(eventValue);
+  };
+
+  const handlePasswordInputChange = (eventValue) => {
+    console.log("password eventValue", eventValue);
+    setPassword(eventValue);
+  };
+
+  //handle點擊登入按鈕時的event，在點擊登入的button時會去呼叫handleClick這個function，handleClick這個function會再去呼叫auth.js裡面的login非同步function
+  const handleClick = async () => {
+    //防止使用者沒有輸入username和password
+    if (username.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+    try {
+      //用AuthContext裡面的非同步login函式，可以拿到success的布林值
+      const success = await login({
+        username,
+        password,
+      });
+
+      //登入成功：從後端回傳回來的 success 是 true，用success的值去顯示我們的提示訊息
+      if (success) {
+        // 登入成功訊息
+        Swal.fire({
+          position: "top",
+          title: "登入成功！",
+          timer: 1000,
+          icon: "success",
+          showConfirmButton: false,
+        });
+        return;
+      }
+      //登入失敗
+      Swal.fire({
+        position: "top",
+        title: "登入失敗！",
+        timer: 1000,
+        icon: "error",
+        showConfirmButton: false,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // 我只要去check每個頁面的isAuthenticated的true或是false，來做切換頁面的動作
+  useEffect(() => {
+    //  驗證有成功的話
+    if (isAuthenticated) {
+      // 頁面跳轉到todo頁面
+      navigate("/admin");
+    }
+    //那如果isAuthenticated不為true的話就不做任何頁面套轉的動作
+  }, [navigate, isAuthenticated]); //因為有用到navigate這個function和isAuthenticated，所以就把它放到useEffect的dependency上
+
   return (
     <div className="h-screen flex bg-gradient-to-r from-sky-950 to-blue-500">
       {/* left part */}
@@ -53,6 +122,8 @@ const AdminLogin = () => {
                 type="email"
                 name="email"
                 placeholder="Email Address"
+                value={username}
+                onChange={(e) => handleEmailInputChange(e.target.value)}
               />
             </div>
 
@@ -76,6 +147,8 @@ const AdminLogin = () => {
                 name="password"
                 id="password"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => handlePasswordInputChange(e.target.value)}
               />
             </div>
 
@@ -100,7 +173,6 @@ const AdminLogin = () => {
                 Don't have an account yet?
               </a>
             </div>
-
           </form>
         </div>
       </div>
